4a08abf135344a3b12348586c233ce9acbb87069
HabibUrahmanViqar <habibviqar2018@gmail.com>
2023-05-24 13:45:47 +0430

    handled auth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://HabibUrahman:habibviqar123@storybooks-dev.ue5idwn.mongodb.net/',
+    googleClientID: '1042118871971-ibhqt0i4pm20ln7p9aaqi9phveblhoc2.apps.googleusercontent.com',
+    googleClientSecret: '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'
+}
